c6604d0c5ea5f8f84e868002b56a15e1c52044b7
Amitabh Kumar <amitabh.kumar2@gmail.com>
2019-12-02 12:59:29 -0500

    adding a change from the feature branch

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 //Add click events to the toggler icons so that they show up and/or hid depending on our need
 https://open.canada.ca/data/en/dataset/3d3ef740-4f02-4e58-9592-357c290666c2 --> crime datasets
 https://www.statcan.gc.ca/eng/developers
+
+var url = 'https://newsapi.org/v2/top-headlines?' +
+          'country=cs&' +
+          'apiKey=7dc0f278dd4541128c558c191e758ae5';
+var req = new Request(url);
+fetch(req)
+    .then(function(response) {
+        console.log(response.json());
+    })
