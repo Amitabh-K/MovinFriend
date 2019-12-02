c254fff17465cbc42ac7523962366d08d12608ba
Amitabh Kumar <amitabh.kumar2@gmail.com>
2019-12-02 15:35:38 -0500

    Local News Article working fine with NYT

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //Add click events to the toggler icons so that they show up and/or hid depending on our need
-https://open.canada.ca/data/en/dataset/3d3ef740-4f02-4e58-9592-357c290666c2 --> crime datasets
-https://www.statcan.gc.ca/eng/developers
-
+//https://open.canada.ca/data/en/dataset/3d3ef740-4f02-4e58-9592-357c290666c2 --> crime datasets
+//https://www.statcan.gc.ca/eng/developers
+/*
 var url = 'https://newsapi.org/v2/top-headlines?' +
           'country=cs&' +
           'apiKey=7dc0f278dd4541128c558c191e758ae5';
@@ -10,3 +10,81 @@ fetch(req)
     .then(function(response) {
         console.log(response.json());
     })
+
+    */
+   
+function loadData() {
+
+    var $body = $('body');
+    var $wikiElem = $('#wikipedia-links');
+    var $nytHeaderElem = $('#nytimes-header');
+    var $nytElem = $('#nytimes-articles');
+
+
+    // clear out old data before new request
+    $wikiElem.text("");
+    $nytElem.text("");
+
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ', ' + cityStr;
+
+
+/*
+    // load streetview
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + address  +  '&key=AIzaSyBCPuq3ShdhdhdhdhCm2NFnQqy0mZ5_L6Uo';
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+
+*/
+
+
+    // load nytimes
+    var nytimesUrl = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${cityStr}&api-key=hnBAPj1FFGUSX5ULP7XmAzyZmoAbiwgA`;
+
+
+    $.getJSON(nytimesUrl, function(data){
+
+        $nytHeaderElem.text('Local News Articles About ' + cityStr);
+
+        articles = data.response.docs;
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">'+
+                '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
+                '<p>' + article.snippet + '</p>'+
+            '</li>');
+        };
+
+    }).error(function(e){
+        $nytHeaderElem.text('Local News Articles Could Not Be Loaded');
+    });
+
+
+
+    // load wikipedia data
+    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
+    var wikiRequestTimeout = setTimeout(function(){
+        $wikiElem.text("failed to get wikipedia resources");
+    }, 8000);
+
+    $.ajax({
+        url: wikiUrl,
+        dataType: "jsonp",
+        jsonp: "callback",
+        success: function( response ) {
+            var articleList = response[1];
+
+            for (var i = 0; i < articleList.length; i++) {
+                articleStr = articleList[i];
+                var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+                $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
+            };
+
+            clearTimeout(wikiRequestTimeout);
+        }
+    });
+
+    return false;
+};
+
+$('#form-container').submit(loadData);
