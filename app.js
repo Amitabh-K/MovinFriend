c47c6fd92520dc1c2d6319c97dfc71ff14b9a3bf
Amitabh Kumar <amitabh.kumar2@gmail.com>
2019-12-05 14:00:26 -0500

    weather sections completed

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ fetch(req)
         console.log(response.json());
     })
 
-    */
-   
+ 
 function loadData() {
 
     var $body = $('body');
@@ -58,4 +57,190 @@ function loadData() {
 $('#form-container').submit(loadData);
 
 
+*/
+
+
+
+$(document).ready(function(){
+    // adding newus api wiht querry also API key
+    $("#submit-btn").on("click",function(e){
+      e.preventDefault();
+      // defining variables
+      var query = $("#city").val();
+      // adding newus api wiht querry also API key
+      var url = "https://newsapi.org/v2/everything?q="+query+"&sortBy=popularity&apiKey=7dc0f278dd4541128c558c191e758ae5";
+      // preventing page
+      if(query !== ""){
+        
+        $.ajax({
+          
+          url: url,
+          method: "GET",
+          dataType: "json",
+          
+          beforeSend: function(){
+            $("#loader").show();
+          },
+          
+          compvare: function(){
+            $("#loader").hide();
+          },
+          // handling response froim the api 
+          success: function(news){
+            var output = "";
+            // creating var called latest news
+            var latestNews = news.articles;
+            
+            for(var i in latestNews){
+              //using backticks so that can write html w/o concatenation
+              output +=`
+                <div class="col l6 m6 s12">
+                <h4>${latestNews[i].title}</h4>
+                <img src="${latestNews[i].urlToImage}" class="responsive-img">
+                <p>${latestNews[i].description}</p>
+                <p>${latestNews[i].content}</p>
+                <p>Published on: ${latestNews[i].publishedAt}</p>
+                <a href="${latestNews[i].url}" class="btn">Read more</a>
+                </div>
+              `;
+            }
+             //till there is data 
+            if(output !== ""){
+              $("#newsResults").html(output);
+               M.toast({
+                html: "There you go, nice reading",
+                classes: 'green'
+              });
+              // once output is blank
+
+            }else{            
+              var noNews = `<div style='text-align:center; font-size:36px; margin-top:40px;'>This news isn't available. Sorry about that.<br>Try searching something else </div>`;
+               $("#newsResults").html(noNews);
+              M.toast({
+                html: "No news for the City",
+                classes: 'red'
+              });
+            }
+            
+          },
+          // handling error froim the api 
+          error: function(){
+             var internetFailure = `
+             <div style="font-size:34px; text-align:center; margin-top:40px;">Please check your internet connection and try again.
+             <img src="img/internet.png" class="responsive-img">
+             </div>`;
+             
+            $("#newsResults").html(internetFailure);
+             M.toast({
+                html: "We encountered an error, please try again",
+                classes: 'red'
+              });
+          }
+          
+          
+        });
+        
+      }else{
+        var missingVal = `<div style="font-size:34px; text-align:center; margin-top:40px;">Please enter any search term in the search engine</div>`;
+        $("#newsResults").html(missingVal);
+         M.toast({
+                html: "Please enter search crieteria",
+                classes: 'red'
+              });
+      }
+
+    
+    
+    
+    
+      
+      
+    });
+
+    
+
+    $("#submit-btn").on("click", function (event) {
+      event.preventDefault();
+  
+      var userInput = $("#city").val();
+  
+      var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&units=imperial" + "&APPID=42289ea57481ffa0e2b1aa22ae9c2d55";
+      $.ajax({
+          url: queryURL,
+          method: "GET"
+      }).then(function (response) {
+          console.log(response);
+          console.log(response.name);
+          console.log(response.main.temp);
+          console.log(response.main.humidity);
+  
+          var currentTime = moment().format('L');;
+  
+          //create a variable that store an icon code
+  
+          var iconCode = response.weather[0].icon;
+          var iconUrl = "https://openweathermap.org/img/w/" + iconCode + ".png";
+          var iconImage = $("<img>").attr('src', iconUrl);
+          iconImage.addClass("icon-image");
+  
+          // Create variable to store city name
+          var name = response.name;
+  
+          // Create an HTML element to store variable city name
+  
+          var elementH3 = $("<p>").text(name + " " + currentTime);
+          elementH3.addClass("title");
+  
+          // append element H3 to Div with classname = search result
+  
+          $(".search-result").append(elementH3);
+          $(".search-result").append(iconImage);
+  
+          //Create a variable to store temperature data
+          var tempF = response.main.temp;
+  
+          //Create an HTML element and assign temperature data to it
+  
+          var elementP1 = $("<p>").html("Temperature: " + tempF + "&#8457;");
+  
+          // append temperature data to Div with classname = search result
+  
+          $(".search-result").append(elementP1);
+  
+  
+          //create a variable to store humidity data
+  
+          var humidity = response.main.humidity;
+  
+          // create a HTML element and assign humidity data to it
+  
+          var elementP2 = $("<p>").text("Humidity: " + humidity + "%");
+  
+          //  append humidity data to Div with classname = search result   
+  
+          $(".search-result").append(elementP2);
+  
+          //create a varialbe to store wind speed value
+  
+          var windSpeed = response.wind.speed;
+  
+          var elementP3 = $("<p>").text("Wind Speed: " + windSpeed + " MPH");
+  
+          $(".search-result").append(elementP3);
+  
+          //set variable for UV index API call
+  
+          $( "#searchbtn" ).click(function() {
+              $( ".search-result" ).empty();
+            });
+  
+      })
+  
+  //
+  
+  })
+  
+  
+
+});
 
